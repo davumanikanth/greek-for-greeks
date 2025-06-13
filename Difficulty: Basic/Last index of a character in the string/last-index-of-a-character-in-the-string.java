8a6ced8d8f76f7e95742cfee8a188b5b3bfa2bf1
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-06-14 00:19:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last index of a character in the string/last-index-of-a-character-in-the-string.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    int LastIndex(String s, char p) {
+        // code here
+        int number=s.lastIndexOf(p);
+        return number;
+    }
+}
