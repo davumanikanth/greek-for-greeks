b8d7bb133f7534dce53788218cfa62f9f86eebf6
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-07-04 14:12:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove characters from alphanumeric string/remove-characters-from-alphanumeric-string.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    String removeCharacters(String s) {
+        // code here
+        s=s.replaceAll("[a-zA-Z]","");
+        return s;
+    }
+}
