7d5d8dbbee75a0ce9f71e5138499f858280b9673
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-07-08 15:12:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/CBA Subsequence/cba-subsequence.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+
+class Solution {
+    String cbaSubsequence(String S, int N) {
+        // Write your code here
+        char[] letter=S.toCharArray();
+        Arrays.sort(letter);
+        String str=new String(letter);
+        return str;
+}
+}
