03ad221ad2a7a0c2c2a672cab4c9c3aff2a23ca1
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-07-09 15:30:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pattern Searching/pattern-searching.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+class Solution {
+    int search(String text, String pat) {
+        // code here
+        if(text.contains(pat))
+        {
+            return 1;
+        }
+        return 0;
+    }
+};
