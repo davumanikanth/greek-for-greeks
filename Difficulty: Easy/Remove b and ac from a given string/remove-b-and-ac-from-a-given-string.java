54c7c48751d17353607f18994b2bae6cb5bd0a07
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-07-12 15:12:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove b and ac from a given string/remove-b-and-ac-from-a-given-string.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+
+class Solution {
+    public String stringFilter(String str) {
+        // Your code goes here
+        
+        str=str.replace("ac","");
+        str=str.replace("b","");
+        return str;
+    }
+}
