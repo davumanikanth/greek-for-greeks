4f2684409c90e0175d7818b5f2e0e7d66a66c56e
manikanth <148421168+davumanikanth@users.noreply.github.com>
2026-01-08 19:31:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
@@ -1,15 +1,15 @@
-class Solution {
-    class pair
+class pair
     {
-        int dist;
-        int nodes;
-        pair(int dist,int nodes)
+        int first ;
+        int second;
+        pair(int first,int second)
         {
-            this.dist=dist;
-            this.nodes=nodes;
-            
+            this.first=first;
+            this.second=second;
         }
     }
+class Solution {
+    
     public int[] dijkstra(int V, int[][] edges, int src) {
         // code here
         ArrayList<ArrayList<pair>> adj=new ArrayList<>();
@@ -17,60 +17,48 @@ class Solution {
         {
             adj.add(new ArrayList<>());
         }
-        
         for(int[] e:edges)
         {
-            int v=e[0];
-            int u=e[1];
+            int u=e[0];
+            int v=e[1];
             int w=e[2];
-            adj.get(v).add(new pair(w,u));
             adj.get(u).add(new pair(w,v));
+            adj.get(v).add(new pair(w,u));
+
         }
-        PriorityQueue<pair> pq=new PriorityQueue<>((x,y)-> x.dist -y.dist);
+        PriorityQueue<pair> pq=new PriorityQueue<>((x,y)-> x.first -y.first);
         int[] distance=new int[V];
+        for(int i=0;i<V;i++)
+        {
+            distance[i]=Integer.MAX_VALUE;
+        }
+        distance[src]=0;
+        pq.add(new pair(0,src));
+        while(!pq.isEmpty())
+        {
+            pair top=pq.remove();
+            int dist=top.first;
+            int node=top.second;
+            for(int i=0;i<adj.get(node).size();i++)
+            {
+                pair it=adj.get(node).get(i);
+                int adjweight=it.first;
+                int adjnode=it.second;
+                if(dist+adjweight<distance[adjnode])
+                {
+                    distance[adjnode]=dist+adjweight;
+                    pq.add(new pair(distance[adjnode],adjnode));
+                    
+                }
+            }
+
+            
+            
+            
+        }
         
-        for(int i=0;i<V;i++) distance[i]=(int)(1e9);
-         pq.add(new pair(0,src));
-         distance[src]=0;
-         while(pq.size()!=0)
-         {
-             
-             int dis=pq.peek().dist;
-             int node=pq.peek().nodes;
-             
-             pq.remove();
-             for(int i=0;i<adj.get(node).size();i++)
-             {
-                 pair it=adj.get(node).get(i);
-                 int adjnode=it.nodes;
-                 int adjweight=it.dist;
-                 if(dis+ adjweight< distance[adjnode])
-                 {
-                     distance[adjnode]=dis+adjweight;
-                     pq.add(new pair(distance[adjnode],adjnode));
-                 }
-             }
-         }
-         return distance;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+                    return distance;
+
         
     }
 }
