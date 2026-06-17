73c8481b99d3335d37aac004863794729609323b
manikanth <148421168+davumanikanth@users.noreply.github.com>
2026-06-17 18:02:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Josephus problem/josephus-problem.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int josephus(int n, int k) {
+        // code here
+        int ans = 0; // 0-indexed
+    for (int i = 2; i <= n; i++) {
+        ans = (ans + k) % i;
+    }
+    return ans + 1;
+    }
+}
